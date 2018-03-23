ca8b73ac01570a14096dbbdce7868793e117ccf8
Liam Prior <liamprior44@gmail.com>
2018-03-23 12:53:18 -0700

    removing comments

--- ft_rotation.c
+++ ft_rotation.c
@@ -6,38 +6,11 @@
 /*   By: lprior <lprior@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/03/14 20:56:05 by lprior            #+#    #+#             */
-/*   Updated: 2018/03/23 12:45:30 by lprior           ###   ########.fr       */
+/*   Updated: 2018/03/23 12:52:57 by lprior           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
-
-// t_plc	*find_points(t_cam *cam, t_plc *current)
-// {
-// 	t_rot	*alg;
-// 	t_plc	*cur;
-
-// 	cur = current;
-// 	alg = (t_rot *)malloc(sizeof(t_rot));
-// 	alg->x0 = cur->x;
-// 	alg->y0 = (cur->y * cos(cam->angle_x)) + (cur->z * sin(cam->angle_x));
-// 	alg->z0 = (cur->z * cos(cam->angle_x)) - (cur->y * sin(cam->angle_x));
-// 	alg->x1 = (alg->x0 * cos(cam->angle_y)) - (alg->z0 * sin(cam->angle_y));
-// 	alg->y1 = alg->y0;
-// 	alg->z1 = (alg->z0 * cos(cam->angle_y)) + (alg->x0 * sin(cam->angle_y));
-// 	alg->x2 = (alg->x1 * cos(cam->angle_z)) + (alg->y1 * sin(cam->angle_z));
-// 	alg->y2 = (alg->y1 * cos(cam->angle_z)) - (alg->x1 * sin(cam->angle_z));
-// 	alg->x2 *= 10;
-// 	alg->y2 *= 10;
-// 	alg->x2 += 150;
-// 	alg->y2 += 150;
-// 	cur->x_place = (int)alg->x2;
-// 	cur->y_place = (int)alg->y2;
-// 	free(alg);
-// 	return (cur);
-// }
-
-
 
 //okay bud im leaking in here!!!!!!!!!!!
 t_links     *ft_lets_get_started(t_links *head, t_rotation *rot, t_env *all)
